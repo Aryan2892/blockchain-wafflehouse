18d483c70c236807b4253777a4ae2335170d2a19
Kyle Angelich <kyleangelich1@gmail.com>
2024-04-20 16:15:37 -0700

    Remove other link boxes on homepage

--- components/Links.tsx
+++ components/Links.tsx
@@ -2,45 +2,15 @@ import styles from '../styles/Home.module.css'
 
 export default function Links() {
   return (
+    /* should this be a button rather than div?  Does this affect accessibility/screen readers? */
     <div className={styles.grid}>
       <a
         href="https://developers.flow.com/tools/fcl-js/tutorials/flow-app-quickstart"
         target="_blank"
         rel="noreferrer"
         className={styles.card}>
-        <h2>FCL Quickstart &rarr;</h2>
-        <p>Get up and running.</p>
-      </a>
-
-      <a
-        href="https://developers.flow.com/tools/fcl-js/reference/api"
-        target="_blank"
-        rel="noreferrer"
-        className={styles.card}>
-        <h2>API Docs &rarr;</h2>
-        <p>Dig deeper into FCL.</p>
-      </a>
-
-      <a
-        href="https://developers.flow.com/tools/fcl-js/reference/discovery"
-        target="_blank"
-        rel="noreferrer"
-        className={styles.card}
-      >
-        <h2>Wallet Discovery &rarr;</h2>
-        <p>Integrate wallets.</p>
-      </a>
-
-      <a
-        href="https://github.com/chasefleming/fcl-next-scaffold"
-        target="_blank"
-        rel="noreferrer"
-        className={styles.card}
-      >
-        <h2>Scaffold Repo &rarr;</h2>
-        <p>
-          View the repo.
-        </p>
+          {/* connect logic to this button */}
+        <h2>Log In With Wallet</h2>
       </a>
     </div>
   )
